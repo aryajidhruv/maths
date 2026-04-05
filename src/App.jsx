f5758acb8972874571b59604d5487a416b5a7a93
aryajidemo <aryajiidhruv@gmail.com>
2026-04-05 13:52:17 +0530

    bottom issuee

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Removed useNavigate here
+import ScrollToTop from './ScrollToTop';
 import LandingPage from './components/LandingPage';
 import SemesterPage from './components/SemesterPage';
 import SubjectDetailsPage from './components/SubjectDetailsPage'; // Or wherever you saved it
@@ -6,6 +7,7 @@ import SubjectDetailsPage from './components/SubjectDetailsPage'; // Or wherever
 function App() {
   return (
     <Router>
+      <ScrollToTop /> 
       <Routes>
         <Route path="/" element={<LandingPage />} />
 
